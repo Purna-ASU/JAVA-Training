89a50a1c43fde5f14be09c1f77ae77f573cefbea
Purna-ASU <ppeddire@asu.edu>
2023-09-13 00:25:29 -0700

    Problem-1

--- src/com/training/assignment1/Student.java
+++ src/com/training/assignment1/Student.java
@@ -1,4 +1,36 @@
 package com.training.assignment1;
 
 public class Student {
+
+    protected String name;
+    protected int age;
+    protected double grade;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public double getGrade() {
+        return grade;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Purna");
+        s1.setAge(23);
+        s1.setGrade(3.5);
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());
+        System.out.println(s1.getGrade());
+    }
 }
